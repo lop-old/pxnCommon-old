1df5dccf0cefd79a0e74b9f080b1de8087bc3822
lorenzo42p <lorenzo42p@f5fbd6b6-c213-558c-f92f-5ab434f62466>
2013-02-17 08:58:20 +0000

    finished pxnVaultLink class - still needs testing, but works changed how pxnVault class works - cleaning up a bit more   added Preload functions   added MakePayment() function
    git-svn-id: http://pxncommon.googlecode.com/svn/trunk@26 f5fbd6b6-c213-558c-f92f-5ab434f62466

---
+++ java/src/com/poixson/pxnCommon/pxnLinks/pxnVaultLink.java
@@ -0,0 +1,269 @@
+package com.poixson.pxnCommon.pxnLinks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import com.poixson.pxnCommon.pxnVault;
+import com.poixson.pxnCommon.BukkitPlugin.pxnPlugin;
+import com.poixson.pxnCommon.Task.pxnTaskThrottled;
+import com.poixson.pxnCommon.dbPool.dbPool;
+import com.poixson.pxnCommon.dbPool.dbPoolConn;
+
+
+public class pxnVaultLink extends pxnTaskThrottled {
+	private static final String CONTAINER_NAME = "Vault Link";
+
+	// instances
+	private static List<pxnVaultLink> links = new ArrayList<pxnVaultLink>();
+
+	// database
+	private dbPool pool;
+	private Economy economy;
+
+	// run count
+	private int runCountAdjust = 0;
+	// balances cache
+	private HashMap<String, Double> balCache = new HashMap<String, Double>();
+
+
+	public pxnVaultLink factory(pxnPlugin plugin, dbPool pool) {
+		synchronized(links) {
+			pxnVaultLink vaultLink = null;
+			// check existing link db's
+			for(pxnVaultLink link : links) {
+				if(link.pool.equals(pool)) {
+					vaultLink = link;
+					break;
+				}
+			}
+			if(vaultLink == null) {
+				vaultLink = new pxnVaultLink(plugin, pool);
+				links.add(vaultLink);
+			}
+			return vaultLink;
+		}
+	}
+
+
+	public pxnVaultLink(pxnPlugin plugin, dbPool pool) {
+		super(plugin, "Update Players");
+		this.pool = pool;
+		this.economy = pxnVault.getEconomy(log);
+//TODO:
+setDelay(1);
+setPeriod(20);
+Start();
+//set task interval
+//set task sleep
+		log.info(CONTAINER_NAME, "Loaded VaultLink");
+	}
+
+
+	@Override
+	protected void runTaskThrottled() {
+//System.out.println("running task");
+		try {
+			// process adjustments queue
+			Update_BalanceAdjust();
+		} catch (Exception e) {
+			log.exception(e);
+		}
+		try {
+			// update online player balances
+			Update_PlayersOnline();
+		} catch (Exception e) {
+			log.exception(e);
+		}
+	}
+
+
+	// process adjustments queue
+	private void Update_BalanceAdjust() {
+		Update_BalanceAdjust(false);
+	}
+	private void Update_BalanceAdjust(boolean processHolds) {
+		String CONTAINER_NAME = "Balance Adjust";
+		runCountAdjust++;
+		if(runCountAdjust >= 10) {
+			runCountAdjust = 0;
+			processHolds = true;
+		}
+		synchronized(balCache) {
+			dbPoolConn dbLoop = pool.getConnLock();
+			dbPoolConn db     = pool.getConnLock();
+			// query adjustments to make
+			dbLoop.Prepare("SELECT "+
+					"`adjust_id`, `username`, `amount` "+
+					"FROM `PSM_BalanceAdjust` "+
+					"WHERE `hold` = ? "+
+					"ORDER BY `adjust_id` ASC "+
+					"LIMIT 100");
+			dbLoop.setBoolean(1, processHolds);
+			dbLoop.Exec();
+			int count = 0;
+			// loop adjustments
+			while(dbLoop.hasNext()) {
+				int adjustId      = dbLoop.getInt("adjust_id");
+				String playerName = dbLoop.getString("username");
+				Double amount     = dbLoop.getDouble("amount");
+				// make adjustment
+				log.debug(CONTAINER_NAME, playerName+" "+Double.toString(amount) );
+				if(pxnVault.MakePayment(log, CONTAINER_NAME, playerName, amount)) {
+					// remove adjustment
+					db.Prepare("DELETE FROM `PSM_BalanceAdjust` "+
+						"WHERE `adjust_id` = ? "+
+						"LIMIT 1");
+					db.setInt(1, adjustId);
+					db.Exec();
+					count++;
+				} else {
+					// put on hold
+					log.warning(CONTAINER_NAME, "Player "+playerName+" doesn't have enough money to cover "+
+						Double.toString(amount)+" ! This is being put on hold for now.");
+					db.Prepare("UPDATE `PSM_BalanceAdjust` SET "+
+						"`hold` = 1 "+
+						"WHERE `adjust_id` = ? "+
+						"LIMIT 1");
+					db.setInt(1, adjustId);
+					db.Exec();
+				}
+				SleepTaskLoop();
+			}
+			// done
+			db.releaseLock();
+			dbLoop.releaseLock();
+			if(count > 0)
+				log.info(CONTAINER_NAME, "Updated [ "+Integer.toString(count)+" ] player balance"+(count>1 ? "s" : ""));
+		}
+	}
+
+
+	// update online player balances
+	private void Update_PlayersOnline() {
+		String CONTAINER_NAME = "Update Balances";
+		synchronized(balCache) {
+			dbPoolConn dbLoop = pool.getConnLock();
+			// loop online players
+			List<Player> playersOnline = new ArrayList<Player>(Arrays.asList(Bukkit.getOnlinePlayers()));
+			for(Player player : playersOnline) {
+				if(!player.isOnline()) continue;
+				// update a players balance
+				String playerName = player.getName();
+				double newBal = economy.getBalance(playerName);
+				double oldBal = getBalance(playerName);
+				// update balance
+				if(oldBal != newBal) {
+					log.info(CONTAINER_NAME, "Updating balance for player "+playerName+" to "+Double.toString(newBal));
+					setBalance(playerName, newBal);
+				}
+				// sleep thread
+				SleepTaskLoop();
+			}
+			// remove offline players from cache
+			Iterator<String> it = balCache.keySet().iterator();
+			while(it.hasNext()) {
+				if(playersOnline.contains( it.next() ))
+					it.remove();
+			}
+			// done
+			dbLoop.releaseLock();
+		}
+	}
+
+
+	// query database for balance
+	private double getBalance(String playerName) {
+		// cached balance
+		if(balCache.containsKey(playerName))
+			return balCache.get(playerName);
+		// query for balance
+		dbPoolConn db = pool.getConnLock();
+		db.Prepare("SELECT `balance` FROM `PSM_Users` WHERE `username` = ? LIMIT 1");
+		db.setString(1, playerName);
+		db.Exec();
+		if(db.hasNext()) {
+			double bal = db.getDouble("balance");
+			db.releaseLock();
+			return bal;
+		}
+		db.releaseLock();
+		// fall back to vault
+		return economy.getBalance(playerName);
+	}
+
+
+	// set balance in database
+	private void setBalance(String playerName, double bal) {
+		dbPoolConn db = pool.getConnLock();
+		db.Prepare("UPDATE `PSM_Users` SET `balance` = ? WHERE `username` = ? LIMIT 1");
+		db.setDouble(1, bal);
+		db.setString(2, playerName);
+		db.Exec();
+		db.releaseLock();
+	}
+
+
+//	// update a players balance
+//	private void Update_PlayerBalance2(Player player) {
+//		Economy economy = getEconomy();
+//		String playerName = player.getName();
+//		double newBalance = economy.getBalance(playerName);
+//		double oldBalance = 0.0;
+//		if(playerBalances.containsKey(playerName))
+//			oldBalance = playerBalances.get(playerName);
+//		else
+//			oldBalance = _getPlayerBalance(playerName);
+//		// no update needed
+//		if(newBalance == oldBalance)
+//			return;
+//System.out.println("new balance for "+playerName+" old:"+Double.toString(oldBalance)+" new:"+newBalance );
+////table: PSM_Users
+//		_setPlayerBalance(playerName, newBalance);
+//	}
+
+
+//	private double _getPlayerBalanceCached(String playerName) {
+//		if(playerBalances.containsKey(playerName))
+//			return playerBalances.get(playerName);
+//		return 0.0;
+//	}
+
+
+//	private double _getPlayerBalance(String playerName) {
+//		db.Prepare("SELECT `balance` FROM `PSM_Users` WHERE `username` = ? LIMIT 1")
+//			.setString(1, playerName)
+//			.Exec();
+//		if(!db.Next())
+//return 0.0;
+//		playerBalances.put(playerName, db.getDouble("balance"));
+//		return db.getDouble("balance");
+//	}
+//	private boolean _setPlayerBalance(String playerName, double balance) {
+//		playerBalances.put(playerName, balance);
+//		db.Prepare("UPDATE `PSM_Users` SET `balance` = ? WHERE `username` = ? LIMIT 1")
+//			.setDouble(1, balance)
+//			.setString(2, playerName)
+//			.Exec();
+//return true;
+//	}
+
+
+//	// database
+//	protected dbPoolConn getDB() {
+//		return pool.getConnLock();
+//	}
+//	// economy
+//	protected Economy getEconomy() {
+//		return pxnVault.getEconomy(plugin.getLog());
+//	}
+
+
+}
--- java/src/com/poixson/pxnCommon/pxnVault.java
+++ java/src/com/poixson/pxnCommon/pxnVault.java
@@ -2,79 +2,99 @@ package com.poixson.pxnCommon;
 
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
 import net.milkbowl.vault.permission.Permission;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
-import com.poixson.pxnCommon.BukkitPlugin.pxnPlugin;
 import com.poixson.pxnCommon.Logger.pxnLogger;
 
 
-public class pxnVault implements Listener {
+public class pxnVault {
+	private static final String CONTAINER_NAME = "Vault";
 
 	private static Permission vaultPermission = null;
 	private static Economy vaultEconomy = null;
 	private static Chat vaultChat = null;
 
-	// logger
-	protected static pxnLogger log;
-
-
-	public pxnVault(pxnPlugin plugin) {
-		pxnVault.log = plugin.getLog();
-	}
-
 
 	// permissions
-	public static Permission getPermission() {
-		synchronized(vaultPermission) {
+	public static synchronized Permission getPermission(pxnLogger log) {
+		if(vaultPermission == null) {
+			RegisteredServiceProvider<Permission> provider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+			if(provider == null) {
+				log.severe(CONTAINER_NAME, "Failed to load a permission plugin!"); return null;}
+			vaultPermission = provider.getProvider();
 			if(vaultPermission == null) {
-				RegisteredServiceProvider<Permission> provider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
-				if(provider == null) {
-					log.severe("Failed to load a permission plugin!"); return null;}
-				vaultPermission = provider.getProvider();
-				if(vaultPermission == null) {
-					log.severe("Failed to load a permission plugin!"); return null;}
-				log.info("Permission plugin loaded: "+vaultPermission.getName());
-			}
+				log.severe(CONTAINER_NAME, "Failed to load a permission plugin!"); return null;}
+			log.info(CONTAINER_NAME, "Permission plugin loaded: "+vaultPermission.getName());
 		}
 		return vaultPermission;
+	}
+	public static boolean PreloadPermissions(pxnLogger log) {
+		return (getPermission(log) != null);
 	}
 
 
 	// economy
-	public static Economy getEconomy() {
-		synchronized(vaultEconomy) {
+	public static synchronized Economy getEconomy(pxnLogger log) {
+		if(vaultEconomy == null) {
+			RegisteredServiceProvider<Economy> provider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+			if(provider == null) {
+			log.severe(CONTAINER_NAME, "Failed to load a economy plugin!"); return null;}
+			vaultEconomy = provider.getProvider();
 			if(vaultEconomy == null) {
-				RegisteredServiceProvider<Economy> provider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
-				if(provider == null) {
-					log.severe("Failed to load a economy plugin!"); return null;}
-				vaultEconomy = provider.getProvider();
-				if(vaultEconomy == null) {
-					log.severe("Failed to load a economy plugin!"); return null;}
-				log.info("Economy plugin loaded: "+vaultEconomy.getName());
-			}
+				log.severe(CONTAINER_NAME, "Failed to load a economy plugin!"); return null;}
+			log.info(CONTAINER_NAME, "Economy plugin loaded: "+vaultEconomy.getName());
 		}
 		return vaultEconomy;
+	}
+	public static boolean PreloadEconomy(pxnLogger log) {
+		return (getEconomy(log) != null);
 	}
 
 
 	// chat
-	public static Chat getChat() {
-		synchronized(vaultChat) {
+	public static synchronized Chat getChat(pxnLogger log) {
+		if(vaultChat == null) {
+			RegisteredServiceProvider<Chat> provider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
+			if(provider == null) {
+				log.severe(CONTAINER_NAME, "Failed to load a chat plugin!"); return null;}
+			vaultChat = provider.getProvider();
 			if(vaultChat == null) {
-				RegisteredServiceProvider<Chat> provider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
-				if(provider == null) {
-					log.severe("Failed to load a chat plugin!"); return null;}
-				vaultChat = provider.getProvider();
-				if(vaultChat == null) {
-					log.severe("Failed to load a chat plugin!"); return null;}
-				log.info("Chat plugin loaded: "+vaultChat.getName());
-			}
+				log.severe(CONTAINER_NAME, "Failed to load a chat plugin!"); return null;}
+			log.info(CONTAINER_NAME, "Chat plugin loaded: "+vaultChat.getName());
 		}
 		return vaultChat;
+	}
+	public static boolean PreloadChat(pxnLogger log) {
+		return (getChat(log) != null);
+	}
+
+
+	// make payment
+	public static boolean MakePayment(pxnLogger log, String playerName, Double amount) {
+		return MakePayment(log, CONTAINER_NAME, playerName, amount);
+	}
+	public static boolean MakePayment(pxnLogger log, String container, String playerName, Double amount) {
+		if(amount == 0.0)
+			return true;
+		EconomyResponse response = null;
+		// deposit
+		if(amount > 0.0)
+			response = getEconomy(log).depositPlayer(playerName, amount);
+		// withdraw
+		else if(amount < 0.0)
+			response = getEconomy(log).withdrawPlayer(playerName, amount * -1.0);
+		if(response == null || !response.transactionSuccess())
+			return false;
+		// log action
+		if(amount > 0.0)
+			log.info(container, "Made payment of "+Double.toString(amount)+" to "+playerName);
+		else if(amount < 0.0)
+			log.info(container, "Took payment of "+Double.toString(amount * -1.0)+" from "+playerName);
+		return true;
 	}
 
 
